2e5891c27d7ccde07204c5c7baaf79d54af35f53
HaiNguyen13898 <93204872+HaiNguyen13898@users.noreply.github.com>
2022-03-31 18:21:00 +0700

    nop bai

--- case_study1/database_resort_furama.sql
+++ case_study1/database_resort_furama.sql
@@ -3,7 +3,7 @@ create database resort_furama;
 use resort_furama;
 
 create table vi_tri (
-ma_vi_tri int auto_increment,
+ma_vi_tri int auto_increment not null,
 ten_vi_tri varchar (45),
 primary key (ma_vi_tri)
 );
@@ -50,11 +50,8 @@ primary key (ma_kieu_thue)
 
 );
 
-
-
 create table khach_hang (
 ma_khach_hang int auto_increment not null,
-ma_loai_khach int,
 ho_ten varchar (45) not null,
 ngay_sinh date not null,
 gioi_tinh bit  default 1 not null,
@@ -62,6 +59,7 @@ so_cmnd varchar (45) not null,
 so_dien_thoai varchar (45) not null,
 email varchar (45),
 dia_chi varchar (45),
+ma_loai_khach int not null,
 primary key (ma_khach_hang),
 foreign key (ma_loai_khach) references loai_khach (ma_loai_khach)
 );
@@ -76,9 +74,9 @@ luong double not null,
 so_dien_thoai varchar (45) not null,
 email varchar (45),
 dia_chi varchar (45),
-ma_vi_tri int,
-ma_trinh_do int,
-ma_bo_phan int,
+ma_vi_tri int not null,
+ma_trinh_do int not null,
+ma_bo_phan int not null,
 primary key (ma_nhan_vien),
 foreign key (ma_vi_tri) references vi_tri (ma_vi_tri),
 foreign key (ma_trinh_do) references trinh_do (ma_trinh_do),
@@ -91,12 +89,12 @@ ten_dich_vu varchar (45) not null,
 dien_tich int,
 chi_phi_thue double not null,
 so_nguoi_toi_da int,
-ma_kieu_thue int,
-ma_loai_dich_vu int,
 tieu_chuan_phong varchar (45),
 mo_ta_tien_nghi_khac varchar (45),
 dien_tich_ho_boi double,
 so_tang int,
+ma_kieu_thue int not null,
+ma_loai_dich_vu int not null,
 primary key (ma_dich_vu),
 foreign key (ma_kieu_thue) references kieu_thue (ma_kieu_thue),
 foreign key (ma_loai_dich_vu) references loai_dich_vu (ma_loai_dich_vu)
@@ -107,9 +105,9 @@ ma_hop_dong int auto_increment not null,
 ngay_lam_hop_dong datetime not null,
 ngay_ket_thuc datetime not null,
 tien_dat_coc double not null,
-ma_nhan_vien int,
-ma_khach_hang int,
-ma_dich_vu int,
+ma_nhan_vien int not null,
+ma_khach_hang int not null,
+ma_dich_vu int not null,
 primary key (ma_hop_dong),
 foreign key (ma_nhan_vien) references nhan_vien (ma_nhan_vien),
 foreign key (ma_khach_hang) references khach_hang (ma_khach_hang),
@@ -119,13 +117,15 @@ foreign key (ma_dich_vu) references dich_vu (ma_dich_vu)
 
 create table hop_dong_chi_tiet (
 ma_hop_dong_chi_tiet int auto_increment not null,
-ma_hop_dong int,
-ma_dich_vu_di_kem int,
 so_luong int not null,
+ma_hop_dong int not null,
+ma_dich_vu_di_kem int not null,
 primary key (ma_hop_dong_chi_tiet),
 foreign key (ma_hop_dong) references hop_dong (ma_hop_dong),
 foreign key (ma_dich_vu_di_kem) references dich_vu_di_kem (ma_dich_vu_di_kem)
 );
+
+
 
 
 
