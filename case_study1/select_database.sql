0f71cfa456dbb8fa513789a28d973222b51de64c
HaiNguyen13898 <93204872+HaiNguyen13898@users.noreply.github.com>
2022-04-03 21:49:54 +0700

    nop bai

--- case_study1/select_database.sql
+++ case_study1/select_database.sql
@@ -14,32 +14,39 @@ from  hop_dong hd inner join khach_hang kh on kh.ma_khach_hang = hd.ma_khach_han
 where kh.ma_loai_khach = 1 group by kh.ho_ten order by count(hd.ma_hop_dong);
 
 
--- task 5 vẫn chưa ra thầy ơi
+-- task 5 
 select kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, 
-hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, sum(dv.chi_phi_thue + hdct.so_luong * dvdk.gia) as "tong_tien"
+hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, sum(dv.chi_phi_thue + coalesce(hdct.so_luong * dvdk.gia,0)) as "tong_tien"
 from hop_dong hd 
 right join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang 
-join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
-join dich_vu dv on hd.ma_dich_vu = hd.ma_dich_vu
-join hop_dong_chi_tiet hdct on  hd.ma_hop_dong = hdct.ma_hop_dong 
-join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem 
+left join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+left join dich_vu dv on hd.ma_dich_vu = hd.ma_dich_vu
+left join hop_dong_chi_tiet hdct on  hd.ma_hop_dong = hdct.ma_hop_dong 
+left join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem 
 group by kh.ma_khach_hang, hd.ma_hop_dong;
 
 
 -- task 6
 select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich,  dv.chi_phi_thue, ldv.ten_loai_dich_vu
-from dich_vu dv join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu 
-join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu 
-where hd.ngay_lam_hop_dong >= '2021-01-01' and  hd.ngay_lam_hop_dong <= '2021-03-31'  
-group by  dv.ma_dich_vu;
- 
--- select ngay_lam_hop_dong from hop_dong where ngay_lam_hop_dong >= '2021-01-01' and ngay_lam_hop_dong <= '2021-03-31' ; 
+from dich_vu dv left join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu 
+left join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu  
+where dv.ten_dich_vu not in ( select dv.ten_dich_vu 
+from dich_vu join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu
+where hd.ngay_lam_hop_dong >= '2021-01-01' and hd.ngay_lam_hop_dong <= '2021-03-31')
+group by dv.ma_dich_vu;
 
  
  -- task 7 
- select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.so_nguoi_toi_da, dv.chi_phi_thue, ldv.ten_loai_dich_vu
- from dich_vu dv join loai_dich_vu ldv;
- 
+ select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.so_nguoi_toi_da, dv.chi_phi_thue, 
+ ldv.ten_loai_dich_vu from dich_vu dv 
+ left join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu
+ left join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+ where  year (hd.ngay_lam_hop_dong) = '2020' and dv.ma_dich_vu not in 
+ ( select dv.ma_dich_vu from dich_vu dv 
+ left join loai_dich_vu ldv on dv.ma_dich_vu = ldv.ma_loai_dich_vu
+ left join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu where  year (hd.ngay_lam_hop_dong) = '2021') 
+ group by dv.ma_dich_vu; 
+  
  
  -- task 8 
  select distinct ho_ten from khach_hang; -- cách 1 
@@ -55,7 +62,17 @@ group by  dv.ma_dich_vu;
 select hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc, 
 ifnull(sum(hdct.so_luong), 0) as so_luong_dich_vu_di_kem -- hoặc có thể dùng coalesce(column_name,0) 
 from hop_dong hd left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong  group by hd.ma_hop_dong;
+
+-- task 11 
+ select dvdk.ma_dich_vu_di_kem, dvdk.ten_dich_vu_kem from dich_vu_di_kem dvdk 
  
+ 
+ -- task 12
+ 
+ 
+ 
+	
+
  
  
  
